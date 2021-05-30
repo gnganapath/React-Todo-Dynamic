8177c4d4185cdaaf725cd1d568fe8219f0cafe7d
gnganapath <gnganapathi@gmail.com>
2021-05-30 17:11:17 +0530

    Add - todo - username-pwd ->class based react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      React Todo App - POC
-    </div>
-  );
+
+class Form extends React.Component {
+  state = {
+    devices: [{check:"", name:"", password:""}],  
+    readonly: false,
+    passwordShown : false
+  }
+handleChange = (e) => {
+    if (["name", "password"].includes(e.target.className) ) {
+      let devices = [...this.state.devices]
+      devices[e.target.dataset.id][e.target.className] = e.target.value.toUpperCase()
+      this.setState({ devices }, () => console.log(this.state.devices))
+    } else {
+      this.setState({ [e.target.name]: e.target.value.toUpperCase() })
+    }
+  }
+addCat = (e) => {
+    this.setState((prevState) => ({
+    devices: [...prevState.devices, {check:"",name:"", password:""}],
+    }));
+  }
+handleSubmit = (e) => { 
+    e.preventDefault() 
+    alert(JSON.stringify(this.state.devices));
 }
 
-export default App;
+setDefaultPwd = (e) =>{
+
+  console.log(this.state.devices[0].password, e.target.checked);
+  let firstPwd = this.state.devices[0].password;
+  this.setState({readonly : e.target.checked});
+
+  if(e.target.checked){
+    this.state.devices.forEach( item =>{
+      item['password'] =firstPwd;
+    })
+    console.log(JSON.stringify(this.state.devices));
+  }
+  else{
+
+  }
+}
+
+pwdChange = (e) =>{
+  let firstPwd = this.state.devices[0].password;
+  if(this.state.readonly === true){   
+    this.state.devices.forEach( item =>{
+      item['password'] =firstPwd;
+    })
+  }
+}
+
+togglePasswordVisiblity = (e) =>{
+  this.setState( {passwordShown : !this.state.passwordShown});
+}
+render() {
+    let { devices} = this.state
+    return (
+      <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
+        {/* <label htmlFor="name">Owner</label> 
+        <input type="text" name="owner" id="owner" value={owner} />
+        <label htmlFor="description">Description</label> 
+        <input type="text" name="description" id="description" value={description} /> */}
+        <button onClick={this.addCat}>Add new cat</button>
+        {
+          devices.map((val, idx)=> {
+            let deviceId = `Device Name-${idx}`, passwordId = `Password-${idx}`,checkId = `check-${idx}`
+            return (
+              <div key={idx}>
+                  <input
+                  type="checkbox"
+                  name={checkId}
+                  data-id={idx}
+                  id={checkId}
+                  value={devices[idx].checked} 
+                  className="check"
+                />
+                <label htmlFor={deviceId}>{`Device #${idx + 1}`}</label>
+                <input
+                  type="text"
+                  name={deviceId}
+                  data-id={idx}
+                  id={deviceId}
+                  value={devices[idx].name} 
+                  className="name"
+                />
+                <label htmlFor={passwordId}>password</label>
+                  { idx === 0 ?
+                  <input
+                    type={this.state.passwordShown ? "text" : "password"}
+                    name={passwordId}
+                    data-id={idx}
+                    id={passwordId}
+                    value={devices[idx].password} 
+                    className="password"
+                    readOnly={ idx === 0 ? `` : `${this.state.readonly}`}
+                    onChange={this.pwdChange}
+                  /> :  
+                    '' }
+                  { idx >0 ?
+                  <input
+                  type={this.state.passwordShown ? "text" : "password"}
+                  name={passwordId}
+                  data-id={idx}
+                  id={passwordId}
+                  value={devices[idx].password} 
+                  className="password"
+                  readOnly={this.state.readonly}
+                  onChange={this.pwdChange}
+                /> : 
+                '' }
+                {idx === 0 ? <><span onClick={this.togglePasswordVisiblity}> pwd Show/Hide:</span>  <input type="checkbox" onChange={this.setDefaultPwd}/> Set password for other devices </>: ''  }
+              </div>
+            )
+          })
+        }
+        <input type="submit" value="Submit" /> 
+      </form>
+    )
+  }
+}
+export default Form;
---
+++ src/catInputs.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+    const [ownerState, setOwnerState] = useState({
+        owner: '',
+        description: '',
+    });
+
+    const handleOwnerChange = (e) => setOwnerState({
+        ...ownerState,
+        [e.target.name]: [e.target.value],
+    });
+
+    const blankCat = { name: '', age: '' };
+    const [catState, setCatState] = useState([
+        { ...blankCat },
+    ]);
+
+    const addCat = () => {
+        setCatState([...catState, { ...blankCat }]);
+    };
+
+    return (
+        <form>
+            <label htmlFor="owner">Owner</label>
+            <input
+                type="text"
+                name="owner"
+                id="owner"
+                value={ownerState.owner}
+                onChange={handleOwnerChange}
+            />
+            <label htmlFor="description">Description</label>
+            <input
+                type="text"
+                name="description"
+                id="description"
+                value={ownerState.description}
+                onChange={handleOwnerChange}
+            />
+            <input
+                type="button"
+                value="Add New Cat"
+                onClick={addCat}
+            />
+            {
+                catState.map((val, idx) => {
+                    const catId = `name-${idx}`;
+                    const ageId = `age-${idx}`;
+                    return (
+                        <div key={`cat-${idx}`}>
+                            <label htmlFor={catId}>{`Cat #${idx + 1}`}</label>
+                            <input
+                                type="text"
+                                name={catId}
+                                data-idx={idx}
+                                id={catId}
+                                className="name"
+                            />
+                            <label htmlFor={ageId}>Age</label>
+                            <input
+                                type="text"
+                                name={ageId}
+                                data-idx={idx}
+                                id={ageId}
+                                className="age"
+                            />
+                        </div>
+                    );
+                })
+            }
+            <input type="submit" value="Submit" />
+        </form>
+    );
+};
+
+export default Form;
